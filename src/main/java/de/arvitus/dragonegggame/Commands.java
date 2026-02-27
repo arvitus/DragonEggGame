5756a150d1b24013e487c999f26df80159923950
3D_MAMA <34866314+arvitus@users.noreply.github.com>
2026-02-27 01:44:18 +0100

    add basic help command

--- src/main/java/de/arvitus/dragonegggame/Commands.java
+++ src/main/java/de/arvitus/dragonegggame/Commands.java
@@ -51,6 +51,9 @@ public class Commands {
                 .addChild(new CommandNode("info", "Get info about the Dragon Egg game", Commands::dragon_egg$info)
                     .withOptionalPermission(Perms.INFO)
                 )
+                .addChild(new CommandNode("help", "View all commands", Commands::dragon_egg$help)
+                    .withOptionalPermission(Perms.HELP)
+                )
         );
     }
 
@@ -156,6 +159,22 @@ public class Commands {
                 .withMaximumPermission(LevelBasedPermissionSet.OWNER))),
             false
         );
+        return 0;
+    }
+
+    private static int dragon_egg$help(CommandContext<CommandSourceStack> context) {
+        var msg = Component.literal("Available commands:").withStyle(Style.EMPTY.withBold(true));
+        for (CommandNode node : ALL) {
+            for (CommandNode actionNode : node.getActionNodes()) {
+                msg.append(String.format(
+                    "  %s%s - %s",
+                    net.minecraft.commands.Commands.COMMAND_PREFIX,
+                    String.join(" ", actionNode.getPath()),
+                    actionNode.description() != null ? actionNode.description() : "No description provided"
+                ));
+            }
+        }
+        context.getSource().sendSuccess(() -> msg, false);
         return 0;
     }
 }
