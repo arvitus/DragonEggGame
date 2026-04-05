c861c25c0e8b09c2138e7e7fddce382360104419
3D_MAMA <34866314+arvitus@users.noreply.github.com>
2026-04-05 23:12:01 +0200

    refactor: update world type to ServerLevel in Data and DragonEggAPI
    (cherry picked from commit df15112fa5a72f2c4aba3890ec5eaa80e6137c9a)

--- src/main/java/de/arvitus/dragonegggame/config/Data.java
+++ src/main/java/de/arvitus/dragonegggame/config/Data.java
@@ -8,7 +8,7 @@ import com.google.gson.annotations.SerializedName;
 import de.arvitus.dragonegggame.api.DragonEggAPI;
 import de.arvitus.dragonegggame.utils.Utils;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.Level;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -22,7 +22,7 @@ import java.util.UUID;
 import static de.arvitus.dragonegggame.DragonEggGame.*;
 
 public class Data {
-    public transient @Nullable Level world;
+    public transient @Nullable ServerLevel world;
     @SerializedName("world")
     public @NotNull String worldId = "minecraft:overworld";
     @SerializedName("entity_uuid")
