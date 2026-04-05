8b6baf1b146ea62276727b03ff7928d1bee25bbe
3D_MAMA <34866314+arvitus@users.noreply.github.com>
2026-04-05 18:34:32 +0200

    fix broken slot handling if used in Singleplayer

--- src/main/java/de/arvitus/dragonegggame/api/DragonEggAPI.java
+++ src/main/java/de/arvitus/dragonegggame/api/DragonEggAPI.java
@@ -107,6 +107,8 @@ public class DragonEggAPI {
         @NotNull Level world,
         @Nullable Entity entity
     ) {
+        if (world.isClientSide()) return;
+
         if (data == null) {
             LOGGER.warn("API not ready, deferring position update");
             deferredUpdates.add(new DeferredUpdate(type, pos, world, entity));
