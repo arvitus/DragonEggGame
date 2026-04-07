ad9c867e82d355d04a0f6b8688108bdae294c2e7
3D_MAMA <34866314+arvitus@users.noreply.github.com>
2026-04-07 17:10:24 +0200

    refactor: simplify getBearer method and extract gameProfileFromUUID

--- src/main/java/de/arvitus/dragonegggame/Placeholders.java
+++ src/main/java/de/arvitus/dragonegggame/Placeholders.java
@@ -22,7 +22,7 @@ import static de.arvitus.dragonegggame.DragonEggGame.CONFIG;
 public class Placeholders {
     public static final Map<Identifier, PlaceholderHandler> PLACEHOLDERS = Map.of(
         modIdentifier("bearer"),
-        (ctx, arg) -> PlaceholderResult.value(APIUtils.getBearer()),
+        (ctx, arg) -> PlaceholderResult.value(APIUtils.getBearerName()),
         modIdentifier("exact_pos"),
         (ctx, arg) -> {
             if (!Permissions.check(ctx.source(), Perms.EXACT_POS_PLACEHOLDER, PermissionLevel.ADMINS))
